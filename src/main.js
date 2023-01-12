ff0eb20d867da843be5fe54cef38f552275537f1
Edgar <edgarfo1996@gmail.com>
2023-01-12 18:19:21 +0100

    new feature

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: proess.env.VUE_APP_DB_URL,
+  databaseURL: process.env.VUE_APP_DB_URL,
   projectId: process.env.VUE_APP_PROJECT_ID,
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_MSG_SENDER_ID,
