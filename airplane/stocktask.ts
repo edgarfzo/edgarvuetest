711a8c174c6fdda29c818c295f30ee110039f468
Edgar <edgarfo1996@gmail.com>
2023-01-23 23:02:37 +0100

    airplane push

--- airplane/stocktask.ts
+++ airplane/stocktask.ts
@@ -1,5 +1,14 @@
 const axios = require('axios')
 
+async function postData (object){
+	const response = await axios.post('https://test-e4100-default-rtdb.europe-west1.firebasedatabase.app/IBM.json',
+	object
+	)
+	// console.log(response)
+	// console.log(response.data)
+
+}
+
 export default async function getStockData () {
 	try {
 		// 👇️ const data: GetUsersResponse
@@ -12,19 +21,27 @@ export default async function getStockData () {
 		  },
 		)
 	
-		console.log(JSON.stringify(data, null, 4))
+		console.log(JSON.stringify(data['Time Series (5min)'], null, 4))
 	
 		// 👇️ "response status is: 200"
 		console.log('response status is: ', status)
-	
-		return data
+		for (let i = 0;JSON.stringify(data['Time Series (5min)']).length ; i++) {
+			const date = Object.keys(data['Time Series (5min)'])[i]
+			const value = data['Time Series (5min)'][Object.keys(data['Time Series (5min)'])[i]]['4. close']
+			const object = {date, value}
+			postData(object)
+		}
+
+
+
+
 	  } catch (error) {
 		if (axios.isAxiosError(error)) {
 		  console.log('error message: ', error.message)
 		  return error.message;
 		} else {
 		  console.log('unexpected error: ', error)
-		  return 'An unexpected error occurred'
+		  //return 'An unexpected error occurred'
 		}
 	  }
 	}
