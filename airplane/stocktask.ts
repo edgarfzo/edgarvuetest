714f67c16a260e9b19bebb11436866f31a42823c
Edgar <edgarfo1996@gmail.com>
2023-01-24 14:55:41 +0100

    airplane push

--- airplane/stocktask.ts
+++ airplane/stocktask.ts
@@ -1,41 +1,42 @@
 const axios = require('axios')
 
-async function postData (object){
-	const response = await axios.post('https://test-e4100-default-rtdb.europe-west1.firebasedatabase.app/IBM.json',
+async function postData (key:string, object:Object){
+	const response = await axios.put(`https://test-e4100-default-rtdb.europe-west1.firebasedatabase.app/IBM/${key}.json`,
 	object
 	)
-	// console.log(response)
-	// console.log(response.data)
+}
 
+function resultHandling (object:Object, interval:string){
+	const value = object[`Time Series (${interval})`]
+	var key = Object.keys(value)[0]
+	const lastValue = value[key]
+	Object.keys(lastValue).forEach(el => {
+		lastValue[el.slice(3)] = lastValue[el]
+		delete lastValue[el]
+		})
+	key = key.toString().replace(/ /g,"_")
+	return {key,lastValue}
 }
 
 export default async function getStockData () {
 	try {
+		const interval = '30min'
 		// 👇️ const data: GetUsersResponse
 		const { data, status } = await axios.get(
-		  `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=UNTV74N295P1JUJJ`,
+		  `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=${interval}&apikey=UNTV74N295P1JUJJ`,
 		  {
 			headers: {
 			  Accept: 'application/json',
 			},
 		  },
 		)
-	
-		console.log(JSON.stringify(data['Time Series (5min)'], null, 4))
-	
+		//console.log(JSON.stringify(data['Time Series (30min)'], null, 4))
+
+		const {key, lastValue} = resultHandling(data, interval)
+		postData(key, lastValue)
 		// 👇️ "response status is: 200"
 		console.log('response status is: ', status)
-		for (let i = 0;JSON.stringify(data['Time Series (5min)']).length ; i++) {
-			const date = Object.keys(data['Time Series (5min)'])[i]
-			const value = data['Time Series (5min)'][Object.keys(data['Time Series (5min)'])[i]]['4. close']
-			const object = {date, value}
-			postData(object)
-		}
-
-
-
-
-	  } catch (error) {
+	  } catch (error:any) {
 		if (axios.isAxiosError(error)) {
 		  console.log('error message: ', error.message)
 		  return error.message;
