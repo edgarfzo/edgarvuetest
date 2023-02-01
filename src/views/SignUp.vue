c3e449d5863b82a6a03f0695de9e6f6c48e87218
Edgar <edgarfo1996@gmail.com>
2023-02-01 18:54:32 +0100

    push

---
+++ src/views/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+  <div class="py-4">
+    <v-img
+      class="mx-auto mb-10"
+      max-width="228"
+      src="../assets/projecta.png"
+    ></v-img>
+  </div>
+  <v-row>
+    <v-col>
+        <LoginCard
+        type="healthcare"></LoginCard>
+    </v-col>
+    <v-col>
+        <LoginCard
+        type="services"></LoginCard>
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+<script>
+import { getAuth, sendSignInLinkToEmail, createUserWithEmailAndPassword } from "firebase/auth"
+import LoginCard from '@/components/LoginCard.vue'
+  export default {
+    name: 'SignInPage',
+    components:{
+        LoginCard
+    }
+  }
+</script>
